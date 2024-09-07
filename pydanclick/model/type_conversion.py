cd5ae9f871adb64882a9aed48103aa21ba5c9f4e
Félix Martel <22093661+felix-martel@users.noreply.github.com>
2024-09-07 17:14:49 +0200

    Fix Mypy (#22)
    * Fix Mypy
    
    * Remove 3.10 constructs in `type_conversion`

--- pydanclick/model/type_conversion.py
+++ pydanclick/model/type_conversion.py
@@ -3,7 +3,7 @@
 import datetime
 import re
 from pathlib import Path
-from typing import Any, List, Literal, Type, TypedDict, Union, cast, get_args, get_origin
+from typing import Any, List, Literal, Optional, Type, TypedDict, Union, cast, get_args, get_origin
 from uuid import UUID
 
 import click
@@ -33,7 +33,7 @@ class PydanclickParamType(click.ParamType):
     def actual_type(self) -> click.ParamType:
         return self._actual_type
 
-    def convert(self, value: Any, param: click.Parameter | None, ctx: click.Context | None) -> Any:
+    def convert(self, value: Any, param: Optional[click.Parameter], ctx: Optional[click.Context]) -> Any:
         if isinstance(value, PydanclickDefault):
             return None
         return self._actual_type.convert(value, param, ctx)
@@ -42,8 +42,8 @@ class PydanclickParamType(click.ParamType):
         return getattr(self._actual_type, attr)
 
 
-def _get_pydanclick_type(field_type):
-    pydanclick_type: Type[click.ParamType] = type(
+def _get_pydanclick_type(field_type: click.ParamType) -> click.ParamType:
+    pydanclick_type: Type[PydanclickParamType] = type(
         "PydanclickParamType",
         (
             PydanclickParamType,
