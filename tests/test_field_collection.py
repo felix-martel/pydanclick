1eda7fe17502918f97ca8ef41d621fd2ae8a1167
Félix Martel <22093661+felix-martel@users.noreply.github.com>
2024-09-08 20:27:51 +0200

    Allow unpacking of lists of submodels

--- tests/test_field_collection.py
+++ tests/test_field_collection.py
@@ -1,5 +1,5 @@
 from pydanclick.model.field_collection import _Field, collect_fields
-from tests.base_models import Bar, Baz, Foo, Obj
+from tests.base_models import Bar, Baz, Foo, Foos, Obj
 
 
 def test_collect_fields():
@@ -17,3 +17,28 @@ def test_collect_fields_with_exclude():
     fields = collect_fields(Obj, excluded_fields=["foo", "bar.b"])
     assert {field.dotted_name for field in fields} == {"bar.a", "bar.baz.c"}
     # TODO: test docstring parsing
+
+
+def test_collect_fields_without_unpack_list():
+    fields = collect_fields(Foos, unpack_list=False)
+    assert fields == [_Field(name="foos", dotted_name="foos", field_info=Foos.model_fields["foos"], parents=("foos",))]
+
+
+def test_collect_fields_with_unpack_list():
+    fields = collect_fields(Foos, unpack_list=True)
+    assert fields == [
+        _Field(
+            name="a",
+            dotted_name="foos.a",
+            field_info=Foo.model_fields["a"],
+            parents=("foos", "a"),
+            unpacked_from="foos",
+        ),
+        _Field(
+            name="b",
+            dotted_name="foos.b",
+            field_info=Foo.model_fields["b"],
+            parents=("foos", "b"),
+            unpacked_from="foos",
+        ),
+    ]
