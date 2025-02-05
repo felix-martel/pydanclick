ad87009db21c5e4349108888cc670b79207db259
Nikita Reznikov <63803175+rnv812@users.noreply.github.com>
2025-02-05 21:17:50 +0300

    Update tests/test_type_conversion.py
    Co-authored-by: Félix Martel <22093661+felix-martel@users.noreply.github.com>

--- tests/test_type_conversion.py
+++ tests/test_type_conversion.py
@@ -19,7 +19,7 @@ def test_get_type_from_field_with_unconstrained_int():
 
 def test_get_type_from_field_with_optional_int():
     class Foo(BaseModel):
-        a: int | None = None
+        a: Optional[int] = None
 
     click_type = _get_type_from_field(Foo.model_fields["a"])
     assert isinstance(click_type, click.types.IntParamType)
